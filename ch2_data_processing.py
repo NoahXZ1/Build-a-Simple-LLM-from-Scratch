6ad278df8a6e6d58d9f9429c624e3710019eac23
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-05-22 11:37:36 -0400

    daliy update: to-2.5-Byte-Pair-Encoding

--- ch2_data_processing.py
+++ ch2_data_processing.py
@@ -31,7 +31,7 @@ for i, item in enumerate(vocab.items()):
     if i >= 50:
         break
 
-class SimpleTokenizerV1: # This is a simple but complete tokenizer class
+class SimpleTokenizerV2: # This is a simple but complete tokenizer class
     def __init__(self, vocab):
         self.str_to_int = vocab  # stores the vocabulary as a class attribute for access in the encode and decode methods
         # creates a inverse vocabublary that maps token IDs back to the original text tokens.
@@ -41,24 +41,68 @@ class SimpleTokenizerV1: # This is a simple but complete tokenizer class
     def encode(self, text):  # process input text into token IDs
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)',text)
         preprocessed = [ item.strip() for item in preprocessed if item.strip()]
+
+        #replace unknown word with<|unk|> token
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>"for item in preprocessed]
+        
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
     
     def decode(self, ids): # converts token IDs back to the text
         text = " ".join([self.int_to_str[i] for i in ids])
 
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text) # remove space before punctuation
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text) # remove space before punctuation
         return text
     
-tokenizer = SimpleTokenizerV1(vocab) # an simple tokenizer instance
+tokenizer = SimpleTokenizerV2(vocab) # an simple tokenizer instance
 text = """"It's the last he painted, you know," 
-           Mrs. Gisburn said with pardonable pride."""
+          Mrs. Gisburn said with pardonable pride."""
 ids = tokenizer.encode(text)
-print(ids)  
+#print(ids)  
 
 print(tokenizer.decode(ids)) #Token ID back to text
 
 #appling the tokenizer to a simple text sample not in the training set
 text = "Hello, do you like the tea?"
 #This will return a keyerror because the training set does not contain the word "Hello", so we need larger training set to cover more words. 
+# print(tokenizer.encode(text))
+#handling unknown tokens
+all_tokens =sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+
+print(len(vocab.items()))
+# print the last 5 tokens
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+# a practical sample of two independent texts
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = "<|endoftext|>".join((text1,text2))
+print(text)
+
+tokenizer =SimpleTokenizerV2(vocab)
+#1130 ID is for the <|endoftext|>, which 2 1130 are for <|unk|>
 print(tokenizer.encode(text))
+
+print(tokenizer.decode(tokenizer.encode(text)))
+
+"""Byte pair encoding(BPE)"""
+#BPE is based on OpenAI's open source library: tiktoken
+from importlib.metadata import version
+import tiktoken
+print("tiktoken version:", version("tiktoken"))
+#initial BPE tokenizer
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces "
+    "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
