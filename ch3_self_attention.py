a9e04d834dc0a0b5190903146f9ec6e821158f41
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-05-31 13:07:43 -0400

    daily update: ch3 implementing self-attention

---
+++ ch3_self_attention.py
@@ -0,0 +1,86 @@
+#This is code implementation of the chapter 3 in book "Build a Large Language Model from Scratch" 
+#begins on 05/29/2026
+#Then comes light
+
+import torch
+#a small simplified self-attention implementation
+inputs = torch.tensor(
+    [[0.43, 0.15, 0.89], # Your      (x^1)
+     [0.55, 0.87, 0.66], # journey   (x^2)
+     [0.57, 0.85, 0.64], # starts    (x^3)
+     [0.22, 0.58, 0.33], # with      (x^4)
+     [0.77, 0.25, 0.10], # one       (x^5)
+     [0.05, 0.80, 0.55]] # step      (x^6)
+) 
+#computing the dot product of the query, x^2, with every other input token
+query = inputs[1]
+attn_scores_2 = torch.empty(inputs.shape[0])
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query)
+print(attn_scores_2)
+#normalization 
+attn_weights_2_tmp=attn_scores_2 / attn_scores_2.sum()
+print("Attention weights:", attn_weights_2_tmp)
+print("Sum:", attn_weights_2_tmp.sum())
+#softmax function for normalizing the attention scores
+def softmax_naive(x):
+    return torch.exp(x)/torch.exp(x).sum(dim=0)
+attn_weights_2_naive = softmax_naive(attn_scores_2)
+print("Attention weights:", attn_weights_2_naive)
+print("Sum:", attn_weights_2_naive.sum())
+#PyTorch's built-in implementation of the softmax function, naive softmax implementation is numerically unstable
+attn_weights_2= torch.softmax(attn_scores_2, dim=0)
+print("Attention weights:", attn_weights_2)
+print("Sum:", attn_weights_2.sum())
+#computing the weighted sum of the value vectors, context vector z^(2)
+query = inputs[1] #the second input token is the query
+context_vec_2=torch.zeros(query.shape)
+for i,x_i in enumerate(inputs):
+    context_vec_2 += attn_weights_2[i]*x_i
+print(context_vec_2)
+#implementing all context vectors 
+attn_scores = torch.empty(6,6)
+for i, x_i in enumerate(inputs):
+    for j, x_j in enumerate(inputs):
+        attn_scores[i,j] = torch.dot(x_i, x_j)
+print(attn_scores)
+#using matrix multiplication(faster than for loop)
+attn_scores = inputs @ inputs.T
+print(attn_scores)
+# normalize 
+attn_weights = torch.softmax(attn_scores, dim=-1)
+print(attn_weights)
+#verify the rows indeed all sum to 1
+row_2_sum = sum([0.1385, 0.2379, 0.2333, 0.1240, 0.1082, 0.1581])
+print("Row 2 sum:", row_2_sum)
+print("All row sums:", attn_weights.sum(dim=-1))
+#compute all context vectors using matrix multiplication
+all_context_vecs = attn_weights @ inputs
+print(all_context_vecs)
+"""-----------------------------------------------------------------3.4 Implement self-attention with multiple heads------------------------------------------------------------------"""
+x_2=inputs[1] #the second input elemnt
+d_in = inputs.shape[1] #the input embedding size, d=3
+d_out = 2 # the output embedding size, d_out = 2
+#initialize the 3 matrices Wq, Wk, Wv
+torch.manual_seed(123)
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+#compute the query, key, and value vectors:
+query_2 = x_2 @ W_query
+key_2 = x_2 @ W_key
+value_2 = x_2 @ W_value
+print(query_2)
+# compute the key and value vectors for all input tokens by matrix multiplication
+keys = inputs @ W_key
+values = inputs @ W_value
+print("keys.shape:", keys.shape)
+print("values.shape:", values.shape)
+#compute the attention scores(by dot product of query(q) with each key(k) of all input tokens)
+#first we compute the attention score of query token
+keys_2 = keys[1]#this is the key vector of the query token itself
+attn_score_22 = query_2.dot(keys_2)
+print(attn_score_22)
+#then generalize the computation to all attention scores by matrix multiplication
+attn_scores_2 = query_2 @ keys.T
+print(attn_scores_2)
