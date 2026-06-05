dd88baabc41a83f4a6b00daf047d359bdfdaacab
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-06-05 14:37:38 -0400

    4.2 implementatio of LayerNorm

--- ch4_Implementation_of_LLM_architecture.py
+++ ch4_Implementation_of_LLM_architecture.py
@@ -78,3 +78,50 @@ logits = model(batch)
 print("Output shape:", logits.shape)
 #the output shape is (2,6,50257) which is (patch_size, seq_len, vocab_size)
 print(logits) 
+"""-----------------------------4.2 Normalizing activations with Layer Normalization---------------------------------"""
+#implement a simpel nerual network layer with 5 inputs and 6 outputs that we apply to 2 inputs examples:
+torch.manual_seed(123)
+batch_example = torch.randn(2,5) #create 2 examples with 5 dims each
+layer = nn.Sequential(nn.Linear(5,6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+#verify the mean and variance before applying LayerNorm
+#dim=-1 or 1 indicates calculate the mean and variance across the column dimension, dim=0 is for calculating acroos the row dimension
+mean = out.mean(dim=-1, keepdim=True) #use keepdim to maintain the same shape for mean and variance with input
+var = out.var(dim=-1, keepdim=True)  #the output without keepdim=True would be a 2D vector like [0.1324, 0.2170] instead of 2*1 matrix like [[0.1324], [0.2170]]
+print("Mean:\n", mean)
+print("Variance:\n", var)
+#applying layer normalization to the layer outputs, including calculating SD
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var=out_norm.var(dim=-1, keepdim=True)
+print("Normalized layer outputs:\n", out_norm)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+#the mean should be close to 0 and variance should be close to 1 after normalization
+print(torch.allclose(mean, torch.zeros_like(mean), atol=1e-7))
+#turn off the scientific natation for better readability
+torch.set_printoptions(sci_mode=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+# a layer normalization class
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+    
+    def forward(self, x):
+        mean=x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        #eps is a small constant to prevent division by zero
+        norm_x = (x-mean)/torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+#try the LayerNorm module and apply it to the batch input
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased = False, keepdim=True)
+print("Means\n", mean)
+print("Variance:\n", var)
