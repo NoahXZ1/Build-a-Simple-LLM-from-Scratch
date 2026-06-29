71bb49400562e76ea821134aaa9dff291dbecd8b
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-06-29 00:35:53 -0400

    inspect of the loaded weights

--- chp5_Pretraining_on_unlabed_data.py
+++ chp5_Pretraining_on_unlabed_data.py
@@ -491,3 +491,6 @@ from gpt_download import download_and_load_gpt2
 settings, params = download_and_load_gpt2(
     model_size="124M", models_dir= "gpt2"
 )
+#inspect the contents of settings and params
+print("Settings:", settings)
+print("Parameter dictionary keys:", params.keys())
