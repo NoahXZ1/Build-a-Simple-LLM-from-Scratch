211139f7e909a1b9e9dd4ee76c2eb7f21c77db88
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-07-04 01:36:03 -0400

    finished chp6.4: initialized model with pretrained weights and loaded into GPTModel, made a quick test for classify spam message

--- chp6_Fine-turning_for_classification.py
+++ chp6_Fine-turning_for_classification.py
@@ -166,3 +166,49 @@ print("Label batch dimensions:", target_batch.shape)
 print(f"{len(train_loader)} training batches")
 print(f"{len(val_loader)} validation batches")
 print(f"{len(test_loader)} test batches")
+
+"""-------------------------------6.4 Initializing the model with pretrained weights----------------------------"""
+#employ the same configurations we used to pretrain unlabeled data
+CHOOSE_MODEL = "gpt2-small (124M)"
+INPUT_PROMPT = "Every effort moves"
+BASE_CONFIG = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "drop_rate": 0.0,
+    "qkv_bias": True
+}
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl(1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25}
+}
+BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+#use the GPT2 model class and weights previously downloaded and chpater 5
+from gpt_download import download_and_load_gpt2
+from chp5_Pretraining_on_unlabed_data import GPTModel, load_weights_into_gpt
+
+model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+settings, params = download_and_load_gpt2(model_size =model_size, models_dir ="gpt2")
+
+model = GPTModel(BASE_CONFIG)
+load_weights_into_gpt(model, params)
+model.eval()
+
+#reuse the text generation utility function from chapter 4, 5 to ensure that the model generates coherent text
+from ch4_Implementation_of_LLM_architecture import generate_text_simple
+from chp5_Pretraining_on_unlabed_data import text_to_token_ids, token_ids_to_text
+
+text_1 = "Every effort moves you"
+token_ids = generate_text_simple(model = model, idx = text_to_token_ids(text_1, tokenizer),max_new_tokens=15, context_size = BASE_CONFIG["context_length"])
+print(token_ids_to_text(token_ids, tokenizer))
+
+#test whether the model can classify spam messages by prompting with instructions now
+text_2 = (
+    "Is the following text 'spam'? Answer with 'yes' or 'no':"
+    " 'You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award.'"
+)
+token_ids = generate_text_simple(model=model, idx=text_to_token_ids(text_2, tokenizer),max_new_tokens=23, context_size =BASE_CONFIG["context_length"])
+print(token_ids_to_text(token_ids, tokenizer))
