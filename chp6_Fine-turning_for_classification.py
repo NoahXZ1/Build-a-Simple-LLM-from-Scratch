28288c363a4c940e65950e2b64b5797db60c49f2
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-07-07 12:02:41 -0400

    implemented using the fine-tuned model to classify spam messages: finished chp6

--- chp6_Fine-turning_for_classification.py
+++ chp6_Fine-turning_for_classification.py
@@ -442,3 +442,44 @@ test_accuracy = calc_accuracy_loader(test_loader, model, device)
 print(f"Training Accuracy: {training_accuracy*100:.2f}%")
 print(f"Validation Accuracy: {validation_accuracy*100:.2f}%")
 print(f"Test Accuracy: {test_accuracy*100:.2f}%")
+
+"""-------------------------------6.8 Using the Model for Spam Detection--------------------------------"""
+#the following function takes a text message as input and returns whether its spam or not using the fine-tuned model. 
+#It encodes input text, truncates or pads it, and passes it to the model to do the classification
+def classify_review(text, model, tokenizer, device, max_length=None, pad_token_id=50256):
+    model.eval()
+    #prepares inputs to the model
+    input_ids= tokenizer.encode(text)
+    #if max_length is not None:
+    supported_context_length = model.pos_emb.weight.shape[0]
+    #truncates sequences if they are too long
+    input_ids = input_ids[:min(max_length, supported_context_length)]
+    #pad sequences to the longest sequence's length
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+    #adds batch dimension
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0)
+
+    with torch.no_grad():
+        logits = model(input_tensor)[:,-1,:] #logits of last output token
+    predicted_model =torch.argmax(logits, dim=-1).item()
+
+    return "spam" if predicted_model == 1 else "not spam"
+#try on 2 samples
+text_1 = (
+    "You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award."
+)
+print(classify_review(text_1, model, tokenizer, device, max_length=train_dataset.max_length))
+
+text_2 = (
+    "Hey, just wanted to check if we're still on"
+    " for dinner tonight? Let me know!"
+)
+print(classify_review(text_2, model, tokenizer, device, max_length=train_dataset.max_length))
+
+#Save the model using torch.save
+torch.save(model.state_dict(), "review_classifier.pth")
+
+"""the model can be reloaded by:"""
+#model_state_dict = torch.load("review_classifier.pth, map_location = device")
+#model.load_state_dict(model_state_dict)
