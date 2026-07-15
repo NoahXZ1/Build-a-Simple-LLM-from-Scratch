d5a1d68c7325bacda3f5f75c315a724e73f140b4
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-07-15 14:12:50 -0400

    replace of padding token impelemented

--- chp7_Fine-tuning_to_follow_instructions.py
+++ chp7_Fine-tuning_to_follow_instructions.py
@@ -135,3 +135,37 @@ def custom_collate_draft_2(batch, pad_token_id=50256, device="cpu"):
 inputs, targets = custom_collate_draft_2(batch)
 print(inputs)
 print(targets)
+#a new custom collate function allows replace the pad_token_id with -100, and max_length to limit the length of the inputs and targets
+def custom_collate_fn(batch, pad_token_id=50256,ignore_index=-100, allowed_max_length=None, device="cpu"):
+    batch_max_length = max(len(item)+1 for item in batch)
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        new_item += [pad_token_id]
+        #pads sequences to max_length
+        padded = (new_item + [pad_token_id] * (batch_max_length - len(new_item)))
+        #trucates the last tokens for inputs
+        inputs = torch.tensor(padded[:-1])
+        #shifts +1 to the right for taregst
+        targets = torch.tensor(padded[1:])
+        #replaces all but the first padding tokens in targets by ignore_index
+        mask=targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+        #trucates the inputs and targets to allowed_max_length if needed (optional)
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+    
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+#try the sample using the earlier collate function
+inputs, targets = custom_collate_fn(batch)
+print(inputs)
+print(targets)
