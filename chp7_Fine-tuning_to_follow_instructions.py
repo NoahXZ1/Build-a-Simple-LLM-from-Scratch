a54d2d01ab4cf251ac654bd6e38f4ec6fefa4856
Xunzhuo(Noah) Zhang <xunzzhang@gmail.com>
2026-07-20 05:32:13 -0700

    implemented generate test set response

--- chp7_Fine-tuning_to_follow_instructions.py
+++ chp7_Fine-tuning_to_follow_instructions.py
@@ -403,3 +403,23 @@ for entry in test_data[:3]:
     print(f"\nCorrect response:\n{entry['output']}")
     print(f"\nModel's response:\n{response_text.strip()}")
     print("------------------------------------")
+
+#generating test set responses
+from tqdm import tqdm
+
+for i, entry in tqdm(enumerate(test_data), total=len(test_data)):
+    input_text = format_input(entry)
+    token_ids = generate(
+        model=model, idx=text_to_token_ids(input_text, tokenizer).to(device),
+        max_new_tokens=256,
+        context_size = BASE_CONFIG["context_length"],
+        eos_id =50256
+    )
+    generated_text = token_ids_to_text(token_ids, tokenizer)
+
+    response_text = (generated_text[len(input_text):].replace("### Response:", "").strip())
+    test_data[i]["model_response"] = response_text
+
+with open("instruction-data-with-response.json", "w") as file:
+    #indent for pretty-printing
+    json.dump(test_data, file, indent=4)
